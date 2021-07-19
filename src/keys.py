0419b92b84bda0d27452a0289f140658c4d8872d
kunansy <kolobov.kirill@list.ru>
2021-07-19 07:31:06 +0300

    keys refactored

--- src/keys.py
+++ src/keys.py
@@ -1,40 +1,9 @@
-ENGLISH_UP_KEYS = {
-    "Q": 'q',
-    "W": 'w',
-    "E": 'e',
-    "R": 'r',
-    "T": 't',
-    "Y": 'y',
-    "U": 'u',
-    "I": 'i',
-    "O": 'o',
-    "P": 'p'
-}
-ENGLISH_MIDDLE_KEYS = {
-    "A": 'a',
-    "S": 's',
-    "D": 'd',
-    "F": 'f',
-    "G": 'g',
-    "H": 'h',
-    "J": 'j',
-    "K": 'k',
-    "L": 'l'
-}
-ENGLISH_DOWN_KEYS = {
-    "Z": 'z',
-    "X": 'x',
-    "C": 'c',
-    "V": 'v',
-    "B": 'b',
-    "N": 'n',
-    "M": 'm'
-}
-ENGLISH_ALPHABET = {
-    **ENGLISH_UP_KEYS,
-    **ENGLISH_MIDDLE_KEYS,
-    **ENGLISH_DOWN_KEYS
-}
+ENGLISH_UP_KEYS = "qwertyuiop"
+ENGLISH_MIDDLE_KEYS = "asdfghjkl"
+ENGLISH_DOWN_KEYS = "zxcvbnm"
+ENGLISH_ALPHABET = f"{ENGLISH_UP_KEYS}" \
+                   f"{ENGLISH_MIDDLE_KEYS}" \
+                   f"{ENGLISH_DOWN_KEYS}"
 RUSSIAN_ALPHABET = 'ё' + ''.join(
     chr(letter_index)
     for letter_index in range(ord('а'), ord('я') + 1)
